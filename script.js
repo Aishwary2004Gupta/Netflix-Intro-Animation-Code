cb839a00c1cfdc70e428567fd112fe96fb1998c6
Aishwary Gupta <aish042004@gmail.com>
2024-12-19 11:32:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const credits = document.getElementById('credits');
-  
+
     // Trigger "show" after 3.5 seconds (adjust based on your animation duration)
     setTimeout(() => {
       credits.classList.add('show');
