212c35174f9bea9986f76c5bd613399a44d040ce
Aishwary Gupta <aish042004@gmail.com>
2024-12-19 11:29:34 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Wait for the page and animations to load
+document.addEventListener("DOMContentLoaded", () => {
+    const credits = document.getElementById('credits');
+  
+    // Trigger "show" after 3.5 seconds (adjust based on your animation duration)
+    setTimeout(() => {
+      credits.classList.add('show');
+    }, 3500);
+  });
+  
